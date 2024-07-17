5d63d6d274ddbdd5d39f32efd68c5d3f41ca6cad
Watermelon SUGAr <95603245+Waterme1on-SUGAr@users.noreply.github.com>
2024-07-18 01:38:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+VANTA.CLOUDS({
+    el: "#cld" ,
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    skyColor: 0x69bad9
+  })
